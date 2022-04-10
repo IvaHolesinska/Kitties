b1d37753fd89062feb0296bff886459dac3055aa
Iva Holešinská <holesinska.iva@gmail.com>
2022-04-10 11:48:56 +0200

    Set the project

---
+++ kitties-exercise/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './styles.css';
+
+const App = () => {
+  return (
+    <>
+      <div>Article</div>
+    </>
+  );
+};
+
+render(<App />, document.getElementById('root'));
